5b241799f5533aaedbd0819bda9422b2d7e8c8f8
Rodrigo Bossini <professorbossini@gmail.com>
2022-08-12 17:53:10 -0300

    apostila03: retomar do bloco de codigo 2.7.3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  return <div>Meu primeiro componente ReactJS</div>
+  return (
+    <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+      <label htmlFor="nome" style={{display: 'block', marginBottom: 4}}>Nome:</label>
+      <input 
+        type="text" 
+        id="nome" 
+        style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none'}}/>
+      <button
+        style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}}
+      >
+        Enviar
+      </button>
+    </div>
+  );
+
 }
 
 ReactDOM.render(  
